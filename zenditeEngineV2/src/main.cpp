7052d2aa9eb8ce4eefdbb3727d56317852d421e1
Izendi <cjwinters1404@gmail.com>
2024-01-16 23:58:05 +0100

    RT in one weeked practice code part 1. Outputting a basic image to a ppm image file"

--- zenditeEngineV2/src/main.cpp
+++ zenditeEngineV2/src/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include "utils.h"
 #include <GLFW/glfw3.h>
 
@@ -11,9 +12,37 @@
 #include "Camera.h"
 
 #include "Helper/Model.h"
+*/
+
+#include <iostream>
 
 int main()
 {
+	int image_width = 256;
+	int image_height = 256;
+
+
+	//Render
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = 0; j < image_height; j++)
+	{
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+		for (int i = 0; i < image_width; i++)
+		{
+			auto r = double(i) / (image_width - 1);
+			auto g = double(j) / (image_height - 1);
+			auto b = 0;
+
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
+
+	std::clog << "\rDone.                 \n";
 
 	return 0;
 };
