ac715eb0bcbac5c30edbd6c37b4d8dae8c01f9c7
Izendi <cjwinters1404@gmail.com>
2024-04-05 14:33:16 +0200

    some basic post processign effect added to the fbo shader.

--- zenditeEngineV2/res/shaders/fbo/fs_BasicFbo.glsl
+++ zenditeEngineV2/res/shaders/fbo/fs_BasicFbo.glsl
@@ -7,5 +7,33 @@ uniform sampler2D screenTexture;
 
 void main()
 {
+    //Default output:
+    /*
     FragColor = texture(screenTexture, TexCoords);
+    */
+
+    //Color Inversion:
+    /*
+    FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0);
+    */
+
+    //Grayscale (all colour brightness assumed even):
+    /*
+    FragColor = texture(screenTexture, TexCoords);
+    float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+
+    FragColor = vec4(average, average, average, 1.0);
+    */
+
+    //Greyscale 2 (where we try to account for diffwerent colors like green apearing brighter than red and blue to the human eye)
+    /*
+    FragColor = texture(screenTexture, TexCoords);
+    float average = (0.299 * FragColor.r + 0.587 * FragColor.g + 0.114 * FragColor.b) / 3.0;
+    average = average * 2; //doubled the average as the scene can be quite dim by default.
+    FragColor = vec4(average, average, average, 1.0);
+    */
+
+    //Kernal Effects:
+
+
 }
